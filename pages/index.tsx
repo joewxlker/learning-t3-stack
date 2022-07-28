35c4921d7368b10ae4d9a169042877a49f3b0753
joseph <joewxlk3r@gmail.com>
2022-07-28 19:42:32 +0800

    converted to web app loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,62 @@
 import Head from 'next/head'
+import Image from 'next/image';
 import { useState } from 'react';
-import Layout from '../components/layout';
 
-export default function Home() {
-
+export default function Home():JSX.Element {
   const [loading, setLoading] = useState(true);
   setTimeout(() => {
     setLoading(false)
-  },800)
+  }, 2000)
+  
+  if (!loading) {
+    window.location.href = '/main'
+  }
 
   return (
-    <div className="container">
-      <Head>
+    <>
+    <Head>
         <title></title>
       </Head>
-
-      <Layout>
-      <main>
-        <div>
-          {loading && <h1 className='spin-on-load'>\</h1>}
-          {!loading && <h1>finished hydrating!</h1>}
+      <div className="wrapper" style={{ width: '100vw', maxHeight: '100vh' }}>
+        <div className='container'>
+          <div className='name'><h3>Joseph Walker</h3></div>
+          
         </div>
-        </main>
-      </Layout>
+        
+        <Image className='background-image' src='/images/loadingscreenone.jpg' alt='' layout='fill'></Image>
+      
+        <style jsx>{`
 
-      <style jsx>{`
+        .background_image{
+          background-position: cover;
+        }
+
+      .container{
+        height: 100%:
+        width: 100%;
+        z-index: 1;
+        position: relative;
+      }
+
+      .name{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        top: 16vh;
+        right: 27vw;
+        width: 9vw;
+        height: 30px;
+        background-color: red;
+        border: 0.1rem solid maroon; 
+        color: white;
+      }
+
+      .background-image{
+        display: absolute;
+        position: absolute;
+        z-index: 1;
+      }
 
       .spin-on-load{
         animation: spin 2s infinite;
@@ -38,114 +70,6 @@ export default function Home() {
           transform: rotate(360deg)
         }
       }
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-        .logo {
-          height: 1em;
-        }
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
         }
       `}</style>
 
@@ -161,7 +85,9 @@ export default function Home() {
         * {
           box-sizing: border-box;
         }
+
       `}</style>
-    </div>
+      </div>
+      </>
   )
 }
