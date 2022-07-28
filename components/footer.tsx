dfeee585a597a19d13ec43a3baefc96ac0ab9a5c
joseph <joewxlk3r@gmail.com>
2022-07-28 19:43:04 +0800

    UI fixes

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,8 +3,29 @@ import { NavLinks } from "./header";
 
 const Footer: FC = () => {
     return (
-        <div className=''>
+        <div className='main'>
             <NavLinks title='github' href='/'></NavLinks> 
+            <NavLinks title='resume' href='/'></NavLinks> 
+            <NavLinks title='linkedin' href='/'></NavLinks> 
+
+            <style jsx>
+                {`
+
+                .main{
+                    position: fixed;
+                    width: 100vw;
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                    background-color: rgba(100,100,100, .2);
+                    z-index: 2;
+                    bottom: 0;
+                }
+                .main:hover{
+                    opacity: 80%;
+                }
+                `}
+            </style>
         </div>
     );
 }
