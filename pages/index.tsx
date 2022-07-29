5fa30f01457073055e9e3ed3119cc4d2f7cebdea
joseph <joewxlk3r@gmail.com>
2022-07-29 17:27:58 +0800

    UI fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head'
 import Image from 'next/image';
 import { useState } from 'react';
@@ -6,7 +7,7 @@ export default function Home():JSX.Element {
   const [loading, setLoading] = useState(true);
   setTimeout(() => {
     setLoading(false)
-  }, 2000)
+  }, 4000)
   
   if (!loading) {
     window.location.href = '/main'
@@ -19,8 +20,13 @@ export default function Home():JSX.Element {
       </Head>
       <div className="wrapper" style={{ width: '100vw', maxHeight: '100vh' }}>
         <div className='container'>
+            {/** Components go here */}
           <div className='name'><h3>Joseph Walker</h3></div>
-          
+          <span className='bottom-span'>
+            <div className='spinner'></div>
+            <h2 style={{textAlign: 'center'}}>Tips...</h2>
+            </span>
+
         </div>
         
         <Image className='background-image' src='/images/loadingscreenone.jpg' alt='' layout='fill'></Image>
@@ -32,12 +38,21 @@ export default function Home():JSX.Element {
         }
 
       .container{
-        height: 100%:
-        width: 100%;
+        height: 100vh !important:
+        width: 100vw;
         z-index: 1;
         position: relative;
       }
 
+      .bottom-span{
+        color: white;
+        position: absolute;
+        padding-top: 4vh;
+        bottom: 0;
+        width: 100vw;
+        height: 20vh;
+        background-color: rgba(200, 200, 200, 0.3);
+      }
       .name{
         display: flex;
         justify-content: center;
@@ -58,7 +73,13 @@ export default function Home():JSX.Element {
         z-index: 1;
       }
 
-      .spin-on-load{
+      .spinner{
+        position: relative;
+        height: 10px;
+        width: 15vh;
+        top: 6vh;
+        left: 3vw;
+        background-color: white;
         animation: spin 2s infinite;
       }
 
@@ -76,6 +97,7 @@ export default function Home():JSX.Element {
       <style jsx global>{`
         html,
         body {
+          background-image: linear-gradient(rgba(10,10,10,1), rgba(100,100,100,1))
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
