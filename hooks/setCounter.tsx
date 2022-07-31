5d68ca7c813db93e494ed928eba4c1f80febcc89
joseph <joewxlk3r@gmail.com>
2022-07-31 17:24:04 +0800

    debugging

--- hooks/setCounter.tsx
+++ hooks/setCounter.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react"
+import { Dispatch, SetStateAction, useEffect, useState } from "react"
+
 
 interface SetCount {
-    count: object;
-    setCount: (data: string,num:number) => void;
+    iteratePValues: number;
+    iterateRepoValues: number;
+    repos: number;
 }
 export const useSetCount = () => {
-    const [count, setCounter] = useState<any>({});
+    const [count, setCounter] = useState<any>({"iteratePValues": 0, "iterateRepoValues": 0, "repos": 0});
     useEffect(() => {
     },[count, setCounter])
-    return [count, (data: string,num:number) => {
-        return setCounter({...count, [data]: num})
+    return [count, (data: string, num: number) => {
+        return setCounter((prev: object) => { return{...prev, [data]: num}})
     }]
 }
