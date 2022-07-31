4e7930f925648e26aa65102f341f68a46d9196b9
joseph <joewxlk3r@gmail.com>
2022-07-31 17:27:00 +0800

    refactoring/added codewars widget to main

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,58 +1,60 @@
 import { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head';
-import Image from 'next/image';
-import { FC, useEffect, useRef, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
+import CodeWars, { CodeWarsData } from '../components/code-wars';
+import { GithubAccountData, GithubSubscribe, IdCard } from '../components/github-id';
 import Layout from '../components/layout'
-import { useSetCount } from '../hooks/setCounter';
 
-export interface GithubAccountData {
-    login: string;
-    id: number;
-    node_id: string;
-    avatar_url: string;
-    gravatar_id: string;
-    html_url: string;
-    public_repos: number;
-    repos_url: string;
-    message: string;
-}
-
-export interface GithubSubscribe {
-    owner: GithubAccountData;
-    repos_url: string;
-}
-
-interface Props {
-    githubAccountData: GithubAccountData;
-    githubSubscribe: GithubSubscribe;
+export interface MainProps {
+    githubAccountData: GithubAccountData | null;
+    githubSubscribe: GithubSubscribe | null;
+    codeWarsData: CodeWarsData | null;
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const fetchOne = await fetch('https://api.github.com/users/riectivnoodes');
-    const fetchTwo = await fetch('https://api.github.com/users/riectivnoodes/subscriptions');
-    let githubAccountData: GithubAccountData | null = await fetchOne.json();
-    let githubSubscribe: GithubSubscribe | null = await fetchTwo.json();
-    if (githubAccountData.message === "API rate limit exceeded for 58.173.11.163. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)") {
+    const github = await fetch('https://api.github.com/users/riectivnoodes');
+    const githubSubs = await fetch('https://api.github.com/users/riectivnoodes/subscriptions');
+    let githubAccountData: GithubAccountData = await github.json();
+    let githubSubscribe: GithubSubscribe = await githubSubs.json();
+    if (githubAccountData.message !== undefined ){
         githubAccountData = null;
         githubSubscribe = null;
     }
-    return { props: {githubAccountData, githubSubscribe} } 
+    const codeWars = await fetch('https://www.codewars.com/api/v1/users/riectivnoodes');
+    let codeWarsData: CodeWarsData | null = await codeWars.json();
+    return { props: {githubAccountData, githubSubscribe, codeWarsData} } 
   }
 
-const Main: NextPage<Props> = ({githubAccountData, githubSubscribe}) => {
+const Main: NextPage<MainProps> = ({githubAccountData, githubSubscribe, codeWarsData}) => {
 
-    const background = useRef(null)
-    const handleLinkClick = async (props) => {
-        // window.location.href = props;
+    const [innerWidthProp, setInnerWidthProp] = useState<number>();
+    const [brighteness, setBrightness] = useState<number>(6);
+
+    const updateWidth = () => setInnerWidthProp(window.innerWidth)
+            useEffect(() => {
+            window.addEventListener('resize', updateWidth)
+           updateWidth()
+           return () => window.removeEventListener('resize', updateWidth)  
+            }, [innerWidthProp, setInnerWidthProp])
+    //This useEffect updates the inner width value
+    //the inner width value is passed to the child components
+
+    const handleLinkClick = (props: number) => {
+        window.scrollTo(props, 0)
+        //this is the call back function used in the child component
+        //this scrolls the page to the value defined in the child component
     }
+
+
     return (
         <>
             <Head>
                 <title></title>
             </Head>
-            <Layout onLinkClick={handleLinkClick}>
+            <Layout onLinkClick={handleLinkClick} innerWidthProp={innerWidthProp}>
                 <>
-                    <video ref={background} className='video' loop autoPlay={true} muted src="mp4/soldierbkg.mp4"
+                    <span className='video-container'>
+                    <video className='video' loop autoPlay={true} muted src="mp4/soldierbkg.mp4"
                         style={{
                             height: '100vh',
                             width: '110vw',
@@ -64,17 +66,49 @@ const Main: NextPage<Props> = ({githubAccountData, githubSubscribe}) => {
 
                     }} >
                     Your browser does not support the video tag.
-            </video>
-                    <div className='container'>
+                        </video>
+                        </span>
+                    <div className='user-data-container'>
                         {/** Components go here */}
                         <IdCard githubAccountData={githubAccountData} githubSubscribe={githubSubscribe} />
+                        <CodeWars data={codeWarsData} />
                     </div>
+                    <div className='main-menu-container'>
+                        <MainMenu/>
+                    </div>
+                    
+                    <div className='overlay' style={{ backgroundColor: `rgba(0,0,0, 0.5)` }}>
+                    <div className='overlay' style={{ backgroundColor: `rgba(255,255,255, 0.0${brighteness})` }}></div>
+                        <Second />
+                        </div>
                     </>
-                </Layout>
+            </Layout>
+            
             <style jsx>
                 {`
+                
+                .video-container{
+                    position: fixed;
+                    z-index: -2;
+                }
+                .overlay{
+                    display: absolute;
+                    position: absolute;
+                    height: 100vh;
+                    width: 400vw;
+                    z-index: -1;
+                }
                 #hide-alert{
                     animation: hidealert 0s 2s;
+                }
+                .main-menu-container{
+                    display: flex;
+                    flex-direction: column;
+                    position: absolute;
+                    left: 2vw;
+                    top: 10vh;
+                    height: 80vh;
+                    width: 45vw;
                 }
                 .alert-rate-limit{
                     display: flex;
@@ -85,7 +119,6 @@ const Main: NextPage<Props> = ({githubAccountData, githubSubscribe}) => {
                     width: 500px;
                     background: rgba(100,100,100, 0.5);
                     color: white;
-                    // animation: slowrender 1s 2s;
                 }
                 @keyframes hidealert{
                     to {
@@ -113,6 +146,8 @@ const Main: NextPage<Props> = ({githubAccountData, githubSubscribe}) => {
             <style jsx global>{`
         html,
         body {
+            overflow: hidden;
+            scroll-behavior: smooth;
             background-color: rgba(16,16,20);
           padding: 0;
           margin: 0;
@@ -124,6 +159,11 @@ const Main: NextPage<Props> = ({githubAccountData, githubSubscribe}) => {
           box-sizing: border-box;
         }
 
+        @keyframes fade {
+            from {opactiy: 0%}
+            to {opacity: 100%}
+        }
+
       `}</style>
         </>
     )
@@ -131,213 +171,93 @@ const Main: NextPage<Props> = ({githubAccountData, githubSubscribe}) => {
 }
 export default Main;
 
-interface Bool {
-    githubData: boolean;
-}
+export const MainMenu: FC = () => {
 
-export const IdCard: FC<Props> = ({ githubAccountData, githubSubscribe }): JSX.Element => {
-    
-    const [count, setCount] = useSetCount();
-    const [bool, setBool] = useState<Bool>({ githubData: true });
-    
-    useEffect(() => {
-        if (githubAccountData === null) return
-        setBool({ ...bool, ['githubData']: false })
-        setCount('cycleRepo', 1)
-    }, [])
-    
-    setInterval(() => {
-        iterateData();
-    }, 10000)
-
-    const closeAlert = () => {
-        setBool({...bool, ['githubData']: false})
-    }
-    const iterateData = () => {
-        if (githubAccountData === null) return
-        const handlePValueData = () => {
-            if (count['iteratePValues'] < 2) return setCount('iteratePValues', count['iteratePValues'] + 1)
-            else return setCount('iteratePValues', 0 )
-        }
-        const handleRepoData = async () => {
-            await getCommitData();
-            if (count['cycleRepo'] < githubAccountData.public_repos - 1) return setCount('cycleRepo', count['cycleRepo'] + 1);
-            else return setCount('cycleRepo', 0 )
-        }
-        handlePValueData();
-        handleRepoData();
-    };
-
-    const getCommitData = async () => {
-        if (githubSubscribe === null) return
-        const res = await fetch(`https://api.github.com/repos/riectivnoodes/${githubSubscribe[count['cycleRepo']].name}/commits`);
-        const data = await res.json();
-        setCount('repos', data.length);
-    }
-
-    const idPText = [
-        `Current repos ${githubAccountData? githubAccountData.public_repos: null} ...`,
-        `Working on node ${githubAccountData? githubAccountData.node_id : null} ...`,
-        `One more visit to ${githubAccountData? githubAccountData.html_url: null} ...`]
-
+    const buttons = [{
+        subName: 'Fight for a chance of survival',
+        name: 'THE GULAG',
+    },{
+        subName: 'Fight for a chance of survival',
+        name: 'BATTLE ROYAL',
+    },{
+        subName: 'Fight for a chance of survival',
+        name: 'ZOMBIES',
+    }]
     return (
         <>
-            <div className='main'>
-                <span className='main-span'>
-                <Image src='/images/propaganda.png' height={120} width={160} />
-                <span className='id-span'>
-                    <h3>[jw]{githubAccountData !== null ? githubAccountData.login: 'Joseph Walker' }</h3>
-                        <div className='slider-container'>
-                            <div className='slider-wrapper'>
-                                <div className="slider" />
-                                </div>
-                    </div>
-                    <span className='todo'>
-                        <svg height="20" width="20">
-                            <circle cx="10" cy="14" r="5" stroke="rgba(100,100,100, 0)" strokeWidth="3" fill="rgba(244,200,0, 0.4)" />
-                            Sorry, your browser does not support inline SVG.
-                        </svg>
-                        <p title='Clicking this will take you to github'>{idPText[count['iteratePValues']]}</p></span>
-                    {githubAccountData === null && <p>no data to show</p>}
-                    {/* <p>{githubAccountData !== null ? `Repo: ${githubSubscribe[count['cycleRepo']].name} Current commits:${count['repos']}` : ''}</p> */}
-                    </span>
-                    </span>
-                {bool['githubData'] === true && <div className='alert-rate-limit'>
-                            <span className='hide-alert' ><h4>you have been rate limited by github. Data from github will not show</h4><button onClick={closeAlert} className='alert-button'>X</button></span>
-                </div>}
-                <div className='blur-id'/>
+            <div className='menu-title-container'>
+                <h1>WELCOME USER</h1>
             </div>
-            <style jsx>{`
+            <div className='menu-button-container'>
+                {buttons.map((data) => {
+                    return (
+                        <button className='menu-button'><h3>{data.subName}</h3><h2>{data.name}</h2></button>
+                )})}
+            </div>
+            <style jsx >
+                {`
 
-            .blur-id{
-                filter: blur(20px);
-                -webkit-filter: blur(20px);
-                display: flex;
-                flex-direction: column;
-                justify-content: stretch;
-                position: absolute;
-                height: 6rem;
-                width: 30rem;
-                top: 0rem;
-                right: 0rem;
-            }
-            .hide-alert{
-                animation: reveal 0.5s;
-                border: none;
-            }
-            .alert-rate-limit{
-                animation: slidefromright 0.5s ease-in 0.5s;
-                border: none;
-            }
-            .main-span{
-                background-color: rgba(100,100,100,0.1);
-                display: flex;
-                flex-direction: row
-                animation: slidefromright 0.5s ease-in;
-            }
-            .todo{
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                padding: 0%:
-                margin: 0%;
-            }
-
-            p{
-                padding: 0%;
-                margin: 0.2rem;
-            }
-            .todo:hover{
-                display: flex;
-                flex-direction: row;
-                align-items: center;
-                color: white;
-                cursor: pointer;
-            }
-            h3{
-                color: white;
-                margin: 0.2rem;
-            }
-            h4{
-                color: white;
-                margin: 0.2rem;
-            }
-            .slider-container{
-                width: 90%;
-                height: 1rem;
-                background-image: linear-gradient(rgba(200,200,200,0.3), rgba(200,200,200,0.05));
-                box-shadow: inset 0.1rem 0.1rem white;
-                padding: 0.1rem 0.15rem ;
-            }
-
-            .slider-wrapper{
-                animation: reveal 0.5s;
-                border: none;
-                height: 0.7rem;
-            }
-            .slider{
-                position: relative;
-                width: 90%;
-                height: inherit;
-                background-color: orange;
-                box-shadow: inset 0 0.3rem 0.8rem 0.05rem red;
-                animation: expand 1s 0.5s;
-                border: none;
-            }
-
-            .id-span{
-                color: rgba(230,230,230, 0.4);
-                display: flex;
-                flex-direction: column;
-                padding: 0 1rem;
-                width: 100%;
-                background-color: rgba(100,100,100,0.1);
-                animation: slidefromright 0.5s;
-            }
-        .main{
-            display: flex;
-            flex-direction: column;
-            justify-content: stretch;
-            position: absolute;
-            width: 30rem;
-            top: 6rem;
-            right: 2rem;
-            border: none;
-        }
-
-        @keyframes reveal {
-            from {
-                border: none;
-                height: 0px;
-                opacity: 0%;
-            }
-            to {
-                border:none;
-                height: 0px;
-                opacity: 0%;
-            }
-        }
-        @keyframes slidefromright{
-            from {
-                opacity: 0%;
-                transform: translateY(300px);
-            }
-            to {
-                opacity: 100%;
-                transform: translateY(0px);
-            }
-        }
-
-        @keyframes expand {
-            from {
-                width: 0px;
-            }
-            to {
-                width: 90%;
-            }
-        }
-
-        `}</style>
+                button{
+                    margin: 0.3rem;
+                }
+                h3{
+                    color: rgba(200,200,200,0.5);
+                    margin-top: 0.1rem;
+                    margin-bottom: 0.3rem;
+                }
+                h2{
+                    margin: 0rem;
+                }
+                .menu-button{
+                    width: 90%;
+                    height: 5rem;
+                    background-color: rgba(200,200,200, 0.1);
+                    border: 2px solid  rgba(200,200,200, 0.3);
+                    color: white;
+                }
+                .menu-button:hover{
+                    filter: blur(0.1px);
+                    -webkit-filter: blur(0.1px);
+                    background-color: rgba(200,200,200, 0.3);
+                    border: 2px solid  rgba(200,160,70, 0.7);
+                    color: orange;
+                    box-shadow: 0rem 0rem 1rem 0.01rem rgba(200,160,70, 0.7);
+                }
+                .menu-button-container{
+                    width: 90%;
+                    color: white;
+                    display: flex;
+                    flex-direction: column;
+                }
+                .menu-title-container{
+                    height: 5rem;
+                    width: 90%;
+                    color: white;
+                    display: flex;
+                    flex-direction: column;
+                }
+                `}
+            </style>
         </>
+    )
+}
+
+export const Second: FC = ({ }): JSX.Element => {
+    return ( 
+        <>
+            <div className='second'>
+                <MainMenu></MainMenu>
+            </div>
+            <style jsx>
+                {`
+                .second{
+                    top: 10vh;
+                    height: 90vh;
+                    width: 100vw;
+                    position: relative;
+                    transform: translateX(100vw);
+                }`}
+            </style>
+            </>
     )
 }
