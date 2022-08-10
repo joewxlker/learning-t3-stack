c9850b961e72aa8217795dc2250c2b09747700fc
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-10 19:42:30 +0800

    added global styles import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { withTRPC } from "@trpc/next";
 import type { AppRouter } from "../server/router";
 import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
+import "../styles/globals.css";
 import { createTRPCClient } from "@trpc/client";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
