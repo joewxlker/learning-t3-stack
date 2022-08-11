42346d8b51150dced140e2e4fd0ea652e5f7859c
joewxlk3r@gmail.com <joewxlk3r@gmail.com>
2022-08-11 19:10:22 +0800

    .

--- pages/mobile.tsx
+++ pages/mobile.tsx
@@ -6,17 +6,17 @@ export interface MobileProps {
 }
 
 const Mobile: NextPage<MobileProps> = () => {
-    
+
     const [innerWidthProp, setInnerWidthProp] = useState<number>();
 
     const updateWidth = () => setInnerWidthProp(window.innerWidth)
-            useEffect(() => {
-                window.addEventListener('resize', updateWidth)
-                if(window.innerWidth > 800){ window.location.href = '/main'}
-           updateWidth()
-           return () => window.removeEventListener('resize', updateWidth)  
-            }, [innerWidthProp, setInnerWidthProp])
-    
+    useEffect(() => {
+        window.addEventListener('resize', updateWidth)
+        if (window.innerWidth > 800) { window.location.href = '/main' }
+        updateWidth()
+        return () => window.removeEventListener('resize', updateWidth)
+    }, [innerWidthProp, setInnerWidthProp])
+
     return (
         <div>
             hello mobile
