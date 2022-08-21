d445227eeb61ab7f91a823f9f38fcf5a4e627e9a
riectivenoodes <joewxlk3r@example.com>
2022-08-21 15:00:39 +0800

    exports as const now

--- hooks/setBooleanValues.tsx
+++ hooks/setBooleanValues.tsx
@@ -1,15 +1,22 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 
-export interface Bool {
-    githubData: boolean;
-    hover: boolean;
-    openEmblemMenu: boolean;
-    setBool: Dispatch<SetStateAction<object>>;
-}
+// interface BooleanObject {
+//     githubData: boolean;
+//     hover: boolean;
+//     openEmblemMenu: boolean;
+// }
+
+// export interface Bool {
+//     githubData: boolean;
+//     hover: boolean;
+//     openEmblemMenu: boolean;
+//     setBool: (target: string) => React.Dispatch<React.SetStateAction<Boo>>
+// }
 
 export const useHandleSetBool = () => {
     const [bool, setBool] = useState<any>({ githubData: true, hover: false, openEmblemMenu: false });
     return [bool, (target: string) => {
         if (bool[target]) { setBool((prev: object) => { return { ...prev, [target]: false } }) }
-    else { setBool((prev: object) => {return { ...prev, [target]: true }})}}]
-}
+        else { setBool((prev: object) => { return { ...prev, [target]: true } }) }
+    }] as const
+} 
