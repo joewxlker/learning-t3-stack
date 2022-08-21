f2de8b35932fdcf4b0f2f8f30e9be80e8c18772e
riectivenoodes <joewxlk3r@example.com>
2022-08-21 14:59:37 +0800

    moved sendgrid route to appRouter, deleted unused routes

--- server/router/index.ts
+++ server/router/index.ts
@@ -1,14 +1,35 @@
 // src/server/router/index.ts
 import { createRouter } from "./context";
-import superjson from "superjson";
-
-import { sendgridRouter } from "./routes";
+import { z } from "zod";
+import { MailDataRequired } from "@sendgrid/mail";
 
 export const appRouter = createRouter()
-
   //appRouter = main router
-  .transformer(superjson)
-  .merge("sendgrid.", sendgridRouter)
+  .mutation('send-email', {
+    input: z.object({
+      email: z.string(),
+      firstname: z.string(),
+      lastname: z.string(),
+      message: z.string()
+    }),
+    async resolve({ ctx, input }) {
+      console.log(input)
+      ctx.sendgrid.setApiKey(process.env.SENDGRID_API_KEY!)
+      try {
+        const contact: MailDataRequired = {
+          to: process.env.OWNER!,
+          from: process.env.EMAIL!,
+          subject: ` New customer contact email sent from ${input.email}`,
+          text: `${input.message}  Sent from tRPC Portfolio`
+        }
+        await ctx.sendgrid.send(contact)
+        return true;
+      } catch (err) {
+        return false
+      }
+    }
+  }
+  )
 // .merge merges all routes with merge into appRouter
 
 // export type definition of API
