19d38d3c37955a4f54e46153502c1e20845e4d76
riectivenoodes <joewxlk3r@example.com>
2022-08-21 14:58:02 +0800

    added lesser argument for decrementing values

--- hooks/setCounter.tsx
+++ hooks/setCounter.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react"
 
 const useSetCount = () => {
-    const [count, setCounter] = useState<any>({ "iteratePValues": 0, "iterateRepoValues": 0, "repos": 0, "shopSlider": 0 });
+    const [count, setCounter] = useState<any>({ "iteratePValues": 0, "iterateRepoValues": 0, "repos": 0, "shopSlider": 0, "index-logo": 0 });
     return [count, (data: string, num: number) => {
         return setCounter((prev: object) => { return { ...prev, [data]: num } })
     }]
@@ -9,7 +9,7 @@ const useSetCount = () => {
 
 export const useIncrementData = () => {
     const [count, setCounter] = useSetCount();
-    return [count, setCounter, (greater: number, target: string, operator: boolean): React.Dispatch<React.SetStateAction<boolean>> => {
+    return [count, setCounter, (lesser: number, greater: number, target: string, operator: boolean): React.Dispatch<React.SetStateAction<boolean>> => {
         //return count/setCount here, no need to import useSetCount outside of useIncrementData
         if (operator) {
             //operator true === increment
@@ -20,9 +20,11 @@ export const useIncrementData = () => {
 
         if (!operator) {
             //operator false === decrement
-            if (count[target] > greater)
+            if (count[target] > greater) {
+
                 return setCounter(target, (count[target] - 1))
-            else return setCounter(target, greater);
+            }
+            else return setCounter(target, lesser);
         };
     }] as const
 };
