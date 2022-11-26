587eb44697cbf4dee3f5d735060cca488057bf07
joseph walker <joewxlk3r@gmail.com>
2022-11-26 09:17:55 +0800

    .

--- hooks/setBooleanValues.tsx
+++ hooks/setBooleanValues.tsx
@@ -1,18 +1,5 @@
 import { useState } from "react";
 
-// interface BooleanObject {
-//     githubData: boolean;
-//     hover: boolean;
-//     openEmblemMenu: boolean;
-// }
-
-// export interface Bool {
-//     githubData: boolean;
-//     hover: boolean;
-//     openEmblemMenu: boolean;
-//     setBool: (target: string) => React.Dispatch<React.SetStateAction<Boo>>
-// }
-
 export const useHandleSetBool = () => {
     const [bool, setBool] = useState<any>({ githubData: true, hover: false, openEmblemMenu: false });
     return [bool, (target: string) => {
